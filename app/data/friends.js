4dc2e07d650dec7b66d4976d6a82ee79f88359fd
BGorson <BGorson32@gmail.com>
2019-03-03 10:08:54 -0600

    added basic info

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+const friends= 
+{
+    "name":"Brandon",
+    "photo":"https://secure.i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
